058a33fc70e8f37390afbf0812c346ce59672af8
euphoria97 <shkrcl@gmail.com>
2018-04-08 22:40:34 +0530

    login form and additional stuff

---
+++ js/main.js
@@ -0,0 +1,76 @@
+/*
+(function ($) {
+ /*
+    "use strict";
+
+    
+    ==================================================================
+    [ Validate ]*/
+/*    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });*/
+
+/*
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+ */   
+/*   function login(){
+
+    }
+*/
+/*})(jQuery);*/
+
+//function{
+//button onclick="window.location.href='/page2'">login</button>}
+
+
+// document.getElementById("loginbtn").onclick = function () {
+//     location.href = "index.html";
+// }
+
+// function asd() {
+//     console.log("ASD")
+//     window.location = "../index.html";
+
+// }
+
+
